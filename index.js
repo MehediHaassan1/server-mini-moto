860a598da66e8894612e17a770f4ec0b38383205
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-28 13:15:22 +0600

    basic mongodb setup done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 
 // middleware
@@ -11,9 +13,41 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.send('Mini Moto server is running')
+    res.send('Mini Moto server is running')
 })
 
+// ----------------- MongoDB  Starts----------------- //
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wc6pepm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+// ----------------- MongoDB  Ends ----------------- //
+
 app.listen(port, () => {
-  console.log(`Mini Moto app listening on port ${port}`)
+    console.log(`Mini Moto app listening on port ${port}`)
 })
