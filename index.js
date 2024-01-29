a8ab480526de80e64d217d4a69646745a35cfad4
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-29 22:05:22 +0600

    jwt token implement done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -9,6 +10,24 @@ require('dotenv').config();
 // middleware
 app.use(cors())
 app.use(express.json());
+
+
+// custom middleware
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
 
 
 
@@ -79,7 +98,9 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/my-toys', async (req, res) => {
+        app.get('/my-toys', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log(decoded);
             const query = req.query.email;
             const filter = { sellerEmail: query }
             const result = await allToysCollection.find(filter).toArray();
@@ -115,6 +136,14 @@ async function run() {
             const filter = { _id: new ObjectId(id) };
             const result = await allToysCollection.deleteOne(filter);
             res.send(result);
+        })
+
+
+        // jwt
+        app.post('/web-access-token', (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+            res.send({ token });
         })
 
 
